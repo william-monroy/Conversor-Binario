cda81a0adfdfb85a26f940bcfd68a39d00211905
William Monroy <A00829796@itesm.mx>
2021-02-08 12:20:49 -0500

    init commit

---
+++ solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    '''String -> ASCII -> binary'''
+    def transform(self, stringToTransform):
+        resultado_final = ''
+        for letra in stringToTransform:
+          numero = ord(letra)
+          resultado = format(numero,'08b')
+          resultado_final = resultado_final + resultado + ' '
+        return resultado_final
+
+print('Codificador Binario 8bits\n')
+stringToTransform = input('Introduce el texto a codificar: ')
+print('Resultado:',Solution().transform(stringToTransform))
+# output en consola: "01010101010101000100010101000011"
