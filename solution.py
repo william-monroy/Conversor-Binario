c9266bfd6b9918167ea5ab35c8205a69756ae565
William Monroy <A00829796@itesm.mx>
2021-02-09 22:53:16 -0500

    commit

--- solution.py
+++ solution.py
@@ -7,6 +7,7 @@ class Solution:
           resultado = format(numero,'08b')
           resultado_final = resultado_final + resultado + ' '
         return resultado_final
+        
 
 print('Codificador Binario 8bits\n')
 stringToTransform = input('Introduce el texto a codificar: ')
